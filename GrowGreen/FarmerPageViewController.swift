58f2cd497785453055d504791f4eb16e7b9bbc58
Rachel Sterneck <rbs41999@me.com>
2016-06-21 20:31:35 -0400

    FarmerPageViewController and PaymentViewController restructured

--- GrowGreen/FarmerPageViewController.swift
+++ GrowGreen/FarmerPageViewController.swift
@@ -34,42 +34,10 @@ class FarmerPageViewController: UIViewController {
     }
     
     
-    @IBAction func submitOrder(sender: AnyObject) {
-        
-        let alertController = UIAlertController(title: "GroGreen", message: "Order complete!", preferredStyle: UIAlertControllerStyle.Alert)
-        
-        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler:        { action in self.performSegueWithIdentifier("orderComplete", sender: nil)}
-            ))
-        
-        self.presentViewController(alertController, animated: true, completion: nil)
-        
-        let farmItem = NSUserDefaults .standardUserDefaults() .objectForKey("farmChosen")!
-        
-        
-        let restUser = NSUserDefaults . standardUserDefaults() .objectForKey("restUser")!
-        
-        let producePrice = farmItem["price"] as! Double
-        
-        let restaurantUid = restUser["uid"] as! String
-        
-        let restaurantName = restUser["name"] as! String
-        
-        let farmUid = farmItem["uid"] as! String
-        
-        let farmString =
-            farmItem["name"] as! String
-        print(farmString)
-        
-        let orderRef = ref.childByAppendingPath("orders")
-        
-        let order = ["produce": produceType, "quantity": quantity, "price": producePrice, "restaurantUid": restaurantUid, "farmUid": farmUid, "restaurantName":restaurantName, "isDone": false]
-        
-        let order1Ref = orderRef.childByAutoId()
-        
-        order1Ref.setValue(order)
-        
-        
+    @IBAction func payNow(sender: AnyObject) {
     }
+ 
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -99,8 +67,9 @@ class FarmerPageViewController: UIViewController {
         nameLabel.text = "Farm name: \(farmString)"
         priceLabel.text = "Price per pound $\(priceString)"
         
+        NSUserDefaults .standardUserDefaults() .setInteger(quantity, forKey: "stepperQuantity")
         
-        //        NSUserDefaults .standardUserDefaults() .setObject(namelabel.text, forKey: "someonesname")
+
         //
         //        namelabel.text = NSUserDefaults .standardUserDefaults() .stringForKey("someonesname")
         
--- GrowGreen/PaymentViewController.swift
+++ GrowGreen/PaymentViewController.swift
@@ -8,12 +8,55 @@
 
 import UIKit
 import Stripe
+import Firebase
 
 class PaymentViewController: UIViewController, STPPaymentCardTextFieldDelegate {
     
+    let ref = Firebase(url: "https://grogreen.firebaseio.com")
+    
+    var quantity = Int()
+    var produceType = String()
+    
     let paymentTextField = STPPaymentCardTextField()
-    @IBOutlet var saveButton: UIButton!
 
+    @IBOutlet weak var saveButton: UIButton!
+    @IBAction func submitOrder(sender: AnyObject) {
+        let alertController = UIAlertController(title: "GroGreen", message: "Order complete!", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler:        { action in self.performSegueWithIdentifier("submitOrder", sender: nil)}
+            ))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
+        let farmItem = NSUserDefaults .standardUserDefaults() .objectForKey("farmChosen")!
+        
+        
+        let restUser = NSUserDefaults . standardUserDefaults() .objectForKey("restUser")!
+        
+        let producePrice = farmItem["price"] as! Double
+        
+        let restaurantUid = restUser["uid"] as! String
+        
+        let restaurantName = restUser["name"] as! String
+        
+        let farmUid = farmItem["uid"] as! String
+        
+        let farmString =
+            farmItem["name"] as! String
+        print(farmString)
+        
+        let orderRef = ref.childByAppendingPath("orders")
+        
+        let order = ["produce": produceType, "quantity": quantity, "price": producePrice, "restaurantUid": restaurantUid, "farmUid": farmUid, "restaurantName":restaurantName, "isDone": false]
+        
+        let order1Ref = orderRef.childByAutoId()
+        
+        order1Ref.setValue(order)
+
+    }
+    
+    
+    
     
     func paymentCardTextFieldDidChange(textField: STPPaymentCardTextField) {
         // Toggle navigation, for example
@@ -27,7 +70,12 @@ class PaymentViewController: UIViewController, STPPaymentCardTextFieldDelegate {
         paymentTextField.frame = CGRectMake(15, 15, CGRectGetWidth(self.view.frame) - 30, 44)
         paymentTextField.delegate = self
         view.addSubview(paymentTextField)
+        
+        quantity = NSUserDefaults .standardUserDefaults() .integerForKey("stepperQuantity")
     
+        produceType = NSUserDefaults .standardUserDefaults() .stringForKey("produceType")!
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
